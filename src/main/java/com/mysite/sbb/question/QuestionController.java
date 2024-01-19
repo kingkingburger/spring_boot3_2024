9275af2ba903e1691716d6c920746237cd83d875
kingkingburger <dnjsalsgh123@gmail.com>
2024-01-19 10:38:13 +0900

    add: create question

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,11 +1,10 @@
 package com.mysite.sbb.question;
 
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +26,16 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String create(@RequestParam("subject") String subject, @RequestParam("content") String content){
+        this.questionService.create(subject,content);
+        return "redirect:/question/list";
     }
 }
