2ea751445b5d5a9130ad8609144389296add7f8e
kingkingburger <dnjsalsgh123@gmail.com>
2024-01-19 10:02:52 +0900

    add: implement prefix in controller

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
+@Controller("question")
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
