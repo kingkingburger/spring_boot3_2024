4c6674afe4747d5e96723ef27994583a4ef703e4
kingkingburger <dnjsalsgh123@gmail.com>
2024-01-31 09:55:39 +0900

    update: delete unused file

--- src/main/java/com/mysite/sbb/awb/repository/AwbRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.sbb.awb.repository;
-
-import com.mysite.sbb.awb.domain.Awb;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AwbRepository extends JpaRepository<Awb, Long> {
-}
