8098806d30b988b31c24fabe580a670006af47f3
kingkingburger <dnjsalsgh123@gmail.com>
2024-02-13 16:28:02 +0900

    update: 테스트에 @Nested 붙여서 문장형태로 테스트 구분, integrationTestSupport로 통합테스트용 설정 클래스 만들기

--- src/test/java/com/mysite/sbb/support/IntegrationTestSupport.java
+++ src/test/java/com/mysite/sbb/support/IntegrationTestSupport.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class IntegrationTestSupport {
 
 }
