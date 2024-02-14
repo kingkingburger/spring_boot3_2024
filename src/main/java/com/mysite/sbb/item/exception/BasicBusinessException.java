42cde1af3600b2a40324b2e23834159d17b0a049
minho <dnjsalsgh123@gmail.com>
2024-02-15 07:19:36 +0900

    update: item 페키지 만들기

--- src/main/java/com/mysite/sbb/item/exception/BasicBusinessException.java
+++ src/main/java/com/mysite/sbb/item/exception/BasicBusinessException.java
@@ -1,10 +1,11 @@
 package com.mysite.sbb.item.exception;
 
+import com.mysite.sbb.basic.exception.BasicErrorCode;
 import lombok.Getter;
 
 @Getter
 public class BasicBusinessException extends RuntimeException {
-    private final BasicErrorCode statusWithCode;
+    private final com.mysite.sbb.basic.exception.BasicErrorCode statusWithCode;
 
     public BasicBusinessException(BasicErrorCode statusWithCode){
         super(statusWithCode.getMessage());
