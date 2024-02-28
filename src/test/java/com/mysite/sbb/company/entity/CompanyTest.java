448d58917957cc7a6d665330c8a26b966aff7085
kingkingburger <dnjsalsgh123@gmail.com>
2024-02-28 14:02:09 +0900

    update: company entity update test code 작성

--- src/test/java/com/mysite/sbb/company/entity/CompanyTest.java
+++ src/test/java/com/mysite/sbb/company/entity/CompanyTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class CompanyTest {
     @Test
     @DisplayName("회사가 생성되는지 확인하는 테스트")
-    void createMember(){
+    void createCompany(){
         CompanyRegisterRequest request = new CompanyRegisterRequest("COM001");
 
         // given
@@ -16,5 +16,20 @@ class CompanyTest {
 
         // when, then
         Assertions.assertThat(company.getCode()).isEqualTo("COM001");
+    }
+
+    @Test
+    @DisplayName("회사를 업데이트 확인하는 테스트")
+    void updateCompany(){
+        CompanyRegisterRequest request = new CompanyRegisterRequest("COM001");
+
+        // given
+        Company company = Company.builder().code(request.code()).build();
+
+        // when
+        company.update("COM002");
+
+        // then
+        Assertions.assertThat(company.getCode()).isEqualTo("COM002");
     }
 }
