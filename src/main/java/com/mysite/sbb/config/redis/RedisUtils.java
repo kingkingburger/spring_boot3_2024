5f22bd2b459bb0526b43e6e094b83010681e11c3
kingkingburger <dnjsalsgh123@gmail.com>
2024-03-27 18:11:26 +0900

    update: getRedisResult 라는 key를 통해 reids의 value를 가져오는 메서드 만듬

--- src/main/java/com/mysite/sbb/config/redis/RedisUtils.java
+++ src/main/java/com/mysite/sbb/config/redis/RedisUtils.java
@@ -12,23 +12,26 @@ public class RedisUtils {
   private final RedisTemplate<String, Object> redisTemplate;
 
   public void setData(String key, String value, Long expiredTime) {
-    System.out.println("key = " + key);
-    System.out.println("value = " + value);
     redisTemplate.opsForValue().set(key, value);
   }
 
   public String getData(String key) {
+    return getRedisResult(key);
+  }
+
+  public void deleteData(String key) {
+    getRedisResult(key);
+    redisTemplate.delete(key);
+  }
+
+  private String getRedisResult(String key) {
+    if (key == null) {
+      throw new NotFoundException("key를 입력하지 않았습니다.");
+    }
     String redisResult = (String) redisTemplate.opsForValue().get(key);
     if (redisResult == null) {
       throw new NotFoundException("Redis에서 해당 키에 대한 데이터를 찾을 수 없습니다." + key);
     }
-    return (String) redisTemplate.opsForValue().get(key);
-  }
-
-  public void deleteData(String key) {
-    if (key == null) {
-      throw new NotFoundException("key를 입력하지 않았습니다.");
-    }
-    redisTemplate.delete(key);
+    return redisResult;
   }
 }
