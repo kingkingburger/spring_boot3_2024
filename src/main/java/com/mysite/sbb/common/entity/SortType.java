cd6c5b7f6fadbea2908ebb660f1e6aa20a70ce80
minho <dnjsalsgh123@gmail.com>
2024-04-17 21:29:34 +0900

    update: 파일 버전 나누기

--- src/main/java/com/mysite/sbb/common/entity/SortType.java
+++
@@ -1,6 +0,0 @@
-package com.mysite.sbb.common.entity;
-
-public enum SortType {
-    POPULAR,
-    LATEST
-}
--- src/main/java/com/mysite/sbb/company/service/dto/request/CompanyRegisterRequest.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.sbb.company.service.dto.request;
-
-public record CompanyRegisterRequest(
-        String code
-) {
-    private static CompanyRegisterRequest of(String code){
-        return new CompanyRegisterRequest(code);
-    }
-}
