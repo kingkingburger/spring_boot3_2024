482ff8d1f5388b413803f0036e58e88ac2c56cd8
minho <dnjsalsgh123@gmail.com>
2024-04-21 16:29:39 +0900

    update: setting embed tomcat

--- embed/src/main/java/hello/embed/EmbedTomcatServletMain.java
+++ embed/src/main/java/hello/embed/EmbedTomcatServletMain.java
@@ -6,17 +6,20 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 
-public class EmbedTomcatServletMain {
-    public static void main(String[] args) {
-        System.out.println("EmbedTomcatServletMain.main");
-        Tomcat tomcat = new Tomcat();
-        Connector connector = new Connector();
-        connector.setPort(9080);
-        tomcat.addConnector(connector);
+import java.io.File;
 
-        Context context = tomcat.addContext("", "/");
-        tomcat.addServlet("", "helloServlet", new HelloServlet());
-        context.addServletMappingDecoded("/hello-servlet", "helloServlet");
-        tomcat.start();
-    }
+public class EmbedTomcatServletMain {
+  public static void main(String[] args) throws LifecycleException {
+    System.out.println("EmbedTomcatServletMain.main");
+
+    Tomcat tomcat = new Tomcat();
+    Connector connector = new Connector();
+    connector.setPort(8080);
+    tomcat.setConnector(connector);
+
+    Context context = tomcat.addContext("", "/");
+    tomcat.addServlet("", "helloServlet", new HelloServlet());
+    context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+    tomcat.start();
+  }
 }
