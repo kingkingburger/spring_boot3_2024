37ed36ed7701756a57fdfef86948ae4100628b33
kingkingburger <dnjsalsgh123@gmail.com>
2024-04-23 10:27:20 +0900

    update: 커스텀한 springBoot 만들어보기

--- embed/src/main/java/hello/spring/HelloConfig.java
+++ embed/src/main/java/hello/spring/HelloConfig.java
@@ -3,7 +3,7 @@ package hello.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration 주석처리(컴포넌트 스캔 쓰려고)
 public class HelloConfig {
 
     @Bean
