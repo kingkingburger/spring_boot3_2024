8492e398459962e1d254dadc5e382647c0a6da21
kingkingburger <dnjsalsgh123@gmail.com>
2024-04-29 11:36:49 +0900

    update: change the name to board => post

--- playground/src/main/java/com/thresh/playground/board/Board.java
+++
@@ -1,9 +0,0 @@
-package com.thresh.playground.board;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-
-@Entity
-public class Board {
-  @Id Long id;
-}
---
+++ playground/src/main/java/com/thresh/playground/domain/post/Post.java
@@ -0,0 +1,21 @@
+package com.thresh.playground.domain.post;
+
+import com.thresh.playground.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+
+@Entity
+public class Post extends BaseTimeEntity {
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  @Lob
+  private String content;
+
+  @Column private Long numOfLove = 0L;
+}
