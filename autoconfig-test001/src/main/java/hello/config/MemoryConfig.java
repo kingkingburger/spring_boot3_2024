9bf95e212aecc83e4252841afae810ce9fe3c6b1
kingkingburger <dnjsalsgh123@gmail.com>
2024-04-30 10:41:00 +0900

    update: @Conditional로 환경설정 분기하는 법

--- autoconfig-test001/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig-test001/src/main/java/hello/config/MemoryConfig.java
@@ -1,16 +1,21 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// @Conditional(MemoryCondition.class) // MemoryCondition의 matches()가 true면 실행
+@ConditionalOnProperty(name = "memory", havingValue = "on") // 환경 정보가 memory=on 이라면 동작, 위 주석과 같은 동작
 public class MemoryConfig {
 
   @Bean
   public MemoryController memoryController() {
-    return new MemoryController(memoryFinder());
+    return new MemoryController(this.memoryFinder());
   }
 
   @Bean
