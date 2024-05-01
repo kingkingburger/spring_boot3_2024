4e1ac79ca3f25455a4ce5ef45b085cfd603cf186
minho <dnjsalsgh123@gmail.com>
2024-05-02 07:20:41 +0900

    update: java의 vm 옵션 추가로 설정값 사용하기

--- external/src/test/java/hello/external/JavaSystemProperties.java
+++ external/src/test/java/hello/external/JavaSystemProperties.java
@@ -7,11 +7,17 @@ import java.util.Properties;
 
 @Slf4j
 public class JavaSystemProperties {
-    public static void main(String[] args) {
-        Properties properties = System.getProperties();
-        for (Object key : properties.keySet()) {
-            log.info("key: {}, value: {}", key, System.getProperty(String.valueOf(key)));
-        }
-        log.info("Java System Properties");
+  public static void main(String[] args) {
+    Properties properties = System.getProperties();
+    for (Object key : properties.keySet()) {
+      log.info("key: {}, value: {}", key, System.getProperty(String.valueOf(key)));
     }
+    log.info("Java System Properties");
+    String url = System.getProperty("url");
+    String username = System.getProperty("username");
+    String password = System.getProperty("password");
+    log.info("url={}", url);
+    log.info("username={}", username);
+    log.info("password={}", password);
+  }
 }
