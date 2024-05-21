ab9265282782033c90ee20155aff59c2f755d479
kingkingburger <dnjsalsgh123@gmail.com>
2024-05-21 09:59:31 +0900

    update: jwt 토큰 유효성 검사 코드 작성

---
+++ playground/src/main/java/com/thresh/playground/global/exception/ProfileApplicationException.java
@@ -0,0 +1,16 @@
+package com.thresh.playground.global.exception;
+
+import lombok.Getter;
+
+// @AllArgsConstructor -> 이걸로는 오류가 난다.
+@Getter
+public class ProfileApplicationException extends RuntimeException {
+
+  private ErrorCode errorCode;
+
+  // 특별한 로직 [message를 가져오는] 생성자가 필요하기 때문에, 직접 생성자를 정의해야 한다.
+  public ProfileApplicationException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+}
