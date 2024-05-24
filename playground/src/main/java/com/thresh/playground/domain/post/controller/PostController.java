0d9e40f0a2e9b3f6914098902e63fd24a0c7e772
kingkingburger <dnjsalsgh123@gmail.com>
2024-05-24 10:48:59 +0900

    update: post의 endpoint 변경

--- playground/src/main/java/com/thresh/playground/domain/post/controller/PostController.java
+++ playground/src/main/java/com/thresh/playground/domain/post/controller/PostController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/api/post")
 @RequiredArgsConstructor
 public class PostController {
   private final PostService postService;
