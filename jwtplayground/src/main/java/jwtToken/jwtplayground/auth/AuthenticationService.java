6eeb10374b4a14dae4d22e32b550d623b275404b
minho <dnjsalsgh123@gmail.com>
2024-05-26 08:08:40 +0900

    update: jwt와 함께 user 정보 생성하는 로직 구현

--- jwtplayground/src/main/java/jwtToken/jwtplayground/auth/AuthenticationService.java
+++ jwtplayground/src/main/java/jwtToken/jwtplayground/auth/AuthenticationService.java
@@ -1,16 +1,37 @@
 package jwtToken.jwtplayground.auth;
 
+import jwtToken.jwtplayground.config.JwtService;
+import jwtToken.jwtplayground.user.Role;
+import jwtToken.jwtplayground.user.User;
+import jwtToken.jwtplayground.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
-    private final AuthenticationManager authenticationManager;
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        // TODO: var 타입은 뭐지?
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authentication(AuthenticationRequest request) {
