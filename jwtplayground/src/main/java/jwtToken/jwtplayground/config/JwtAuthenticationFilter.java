1b66ffbf4186bd2d0f4fed4c9cf465c92a78137f
minho <dnjsalsgh123@gmail.com>
2024-05-26 06:38:17 +0900

    update: jwt 토큰인지 인증할만한 필터 만들기

---
+++ jwtplayground/src/main/java/jwtToken/jwtplayground/config/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package jwtToken.jwtplayground.config;
+
+// jwt의 유효성에 대해 검사합니다.
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter { // http 요청당 1번의 필터링이 적용되요
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+            
+    }
+}
