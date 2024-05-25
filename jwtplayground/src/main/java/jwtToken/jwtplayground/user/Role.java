a83710d20960891fe40f1111c54a38dc7515b446
minho <dnjsalsgh123@gmail.com>
2024-05-26 06:33:45 +0900

    update: user의 repository 사용

---
+++ jwtplayground/src/main/java/jwtToken/jwtplayground/user/Role.java
@@ -0,0 +1,6 @@
+package jwtToken.jwtplayground.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ playground/src/main/java/com/thresh/playground/domain/user/entity/Role.java
@@ -0,0 +1,5 @@
+package com.thresh.playground.domain.user.entity;
+
+public enum Role {
+    USER, ADMIN
+}
