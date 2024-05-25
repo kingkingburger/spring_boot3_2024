5b0e7bb8b6000b0bcbae7826f9e600cdc1e23825
minho <dnjsalsgh123@gmail.com>
2024-05-26 06:56:25 +0900

    update: jwt 토큰의 키를 만드는 부분과 decode하는 메서드 생성

--- jwtplayground/src/main/java/jwtToken/jwtplayground/config/JwtService.java
+++ jwtplayground/src/main/java/jwtToken/jwtplayground/config/JwtService.java
@@ -1,11 +1,33 @@
 package jwtToken.jwtplayground.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECKET_KEY="7a3da8225db06e6259669441cdee2db7de9e2e8cade3ad55ac683b39fe7ae9f8"
+
     public String extractUsername(String token) {
         return null;
-
     }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().
+                setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECKET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
