5c6308f6ae2ebe127a54206e4e70e2dd1ccf729d
minho <dnjsalsgh123@gmail.com>
2024-05-26 06:58:40 +0900

    update: jwt 토큰에 있는 정보(claim)을 추출하기 위한 메서드 생성

--- jwtplayground/src/main/java/jwtToken/jwtplayground/config/JwtService.java
+++ jwtplayground/src/main/java/jwtToken/jwtplayground/config/JwtService.java
@@ -7,14 +7,21 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    private static final String SECKET_KEY="7a3da8225db06e6259669441cdee2db7de9e2e8cade3ad55ac683b39fe7ae9f8"
+    private static final String SECKET_KEY = "7a3da8225db06e6259669441cdee2db7de9e2e8cade3ad55ac683b39fe7ae9f8";
 
     public String extractUsername(String token) {
         return null;
+    }
+
+    // jwt 토큰에 있는 정보(claim)을 추출하기 위한 메서드
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
