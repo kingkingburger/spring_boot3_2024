af3620d4fa017c76b0081da2fc3c3bd081658a74
kingkingburger <dnjsalsgh123@gmail.com>
2024-06-13 17:58:22 +0900

    update: fetch.lazy, batch_size 조정으로 n+1 문제 완화

--- playground/src/main/java/com/thresh/playground/domain/comment/entity/Comment.java
+++ playground/src/main/java/com/thresh/playground/domain/comment/entity/Comment.java
@@ -4,28 +4,28 @@ import com.thresh.playground.domain.post.entity.Post;
 import com.thresh.playground.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.BatchSize;
 
 import java.util.List;
 
 @Entity
 @Data
+@BatchSize(size = 10)
 public class Comment extends BaseTimeEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    private String content;
+  private String content;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
+  private Post post;
 
-    @ManyToOne
-    @JoinColumn(name = "post_id")
-    private Post post;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_id")
+  private Comment parent;
 
-    @ManyToOne
-    @JoinColumn(name = "parent_id")
-    private Comment parent;
-
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> replies;
-
+  @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Comment> replies;
 }
