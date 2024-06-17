b6ffddb3709310975988ad851e16236290b4acb2
minho <dnjsalsgh123@gmail.com>
2024-06-17 20:47:25 +0900

    update: initialize postService test code

---
+++ playground/src/test/java/com/thresh/playground/domain/post/service/PostServiceTest.java
@@ -0,0 +1,20 @@
+package com.thresh.playground.domain.post.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    void getAllPost() {
+    }
+
+    @Test
+    void savePost() {
+    }
+
+    @Test
+    void getPostById() {
+    }
+}
