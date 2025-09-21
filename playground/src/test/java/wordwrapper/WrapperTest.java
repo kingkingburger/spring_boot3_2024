b05e0d2e5044e503ae679c546f602a9f0a4f32bb
minho <dnjsalsgh123@gmail.com>
2025-09-21 09:02:24 +0900

    update: 최소한의 코드로 테스트 통과하는 경우 추가

--- playground/src/test/java/wordwrapper/WrapperTest.java
+++ playground/src/test/java/wordwrapper/WrapperTest.java
@@ -6,12 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class WrapperTest {
+
   @Test
   public void should_wrap() {
     assertEquals("word\rword", wrap("word word", 4));
     assertEquals("a dog", wrap("a dog", 5));
   }
+
   private String wrap(String text, int length) {
-    return text.replaceAll(" ", "\r");
+    return text.length() > length ? text.replaceAll(" ", "\r") : text;
   }
 }
