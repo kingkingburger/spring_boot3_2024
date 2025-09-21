6cc5c8d6ce7a35a3a32890f91056c60fbf946302
minho <dnjsalsgh123@gmail.com>
2025-09-21 09:06:22 +0900

    update: 빈 문자열을 넣으면 빈 문자열이 나온다

--- playground/src/test/java/wordwrapper/WrapperTest.java
+++ playground/src/test/java/wordwrapper/WrapperTest.java
@@ -10,6 +10,7 @@ public class WrapperTest {
   @Test
   public void should_wrap() {
     assertEquals("", wrap(null, 1));
+    assertEquals("", wrap("", 1));
   }
 
   private String wrap(String text, int length) {
